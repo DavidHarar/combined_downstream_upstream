5710f48bf83f518d6538e79e32353bf331033379
DavidHarar <dudu.harar@gmail.com>
2024-11-01 10:09:57 -0400

    ADD: more experiments on varying reconstruction loss

--- shell_files/ptb-removed_channel-no_added_noise-impute_only_missing-reconstruction_loss.sh
+++ shell_files/ptb-removed_channel-no_added_noise-impute_only_missing-reconstruction_loss.sh
@@ -1,5 +1,5 @@
-python3 /home/david/Desktop/projects/thesis/combined_downstream_upstream/code/ptb-turned_off_channels-no_added_noise-impute_only_missing_multitask-with-varying-reconstruction.py  --num_channels_to_turn_off 0
-python3 /home/david/Desktop/projects/thesis/combined_downstream_upstream/code/ptb-turned_off_channels-no_added_noise-impute_only_missing_multitask-with-varying-reconstruction.py  --num_channels_to_turn_off 1
+# python3 /home/david/Desktop/projects/thesis/combined_downstream_upstream/code/ptb-turned_off_channels-no_added_noise-impute_only_missing_multitask-with-varying-reconstruction.py  --num_channels_to_turn_off 0
+# python3 /home/david/Desktop/projects/thesis/combined_downstream_upstream/code/ptb-turned_off_channels-no_added_noise-impute_only_missing_multitask-with-varying-reconstruction.py  --num_channels_to_turn_off 1
 python3 /home/david/Desktop/projects/thesis/combined_downstream_upstream/code/ptb-turned_off_channels-no_added_noise-impute_only_missing_multitask-with-varying-reconstruction.py  --num_channels_to_turn_off 2
 python3 /home/david/Desktop/projects/thesis/combined_downstream_upstream/code/ptb-turned_off_channels-no_added_noise-impute_only_missing_multitask-with-varying-reconstruction.py  --num_channels_to_turn_off 3
 python3 /home/david/Desktop/projects/thesis/combined_downstream_upstream/code/ptb-turned_off_channels-no_added_noise-impute_only_missing_multitask-with-varying-reconstruction.py  --num_channels_to_turn_off 4
